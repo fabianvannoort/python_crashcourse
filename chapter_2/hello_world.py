ce2ebd36ea04aa6179cc2a48a59ed4a574fe3d34
fabianvannoort <fabian.vn@gmail.com>
2024-11-28 16:27:00 +0100

    Working with strings and variables.

---
+++ chapter_2/hello_world.py
@@ -0,0 +1,13 @@
+print("Hello, Python World!")
+
+# Using a variable.
+message = "Hello again, Python World!"
+print(message)
+
+# Strings may be in single or double quotes.
+message = 'Hello, Python and Crash Course World!'
+print(message)
+
+# Python interpreter will try to assist with typos in variable names. 
+message = "Hello, Python and Crash Course World!"
+#print(mesage)
